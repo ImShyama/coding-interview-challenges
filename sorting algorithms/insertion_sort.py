650f9e01e43ec3ed7fb2c2df766605668443836f
Shyama <p.shyama96@gmail.com>
2020-07-06 22:24:23 +0545

    sorting algorithm

---
+++ sorting algorithms/insertion_sort.py
@@ -0,0 +1,20 @@
+# swap function for swaping the array element
+def swap(array, i, j):
+    temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+
+
+# Insertion sort function which sort the array
+def insertion_sort(array):
+    for i in range(0, len(array)):
+        j = i
+        while j > 0 and array[j-1] > array[j]:
+            swap(array, j-1, j)
+            j -= 1
+
+
+# Creating list and calling insertion sort function
+array = [2, 5, 10, 4, 6]
+insertion_sort(array)
+print(array)
